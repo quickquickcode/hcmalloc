2ff2eb204519c9d456ea313ef9f5dda258bf15b2
勇敢冲就对 <2859201503@qq.com>
2023-05-07 03:44:45 +0000

    Replace size.h

--- src/size.h
+++ src/size.h
@@ -1,4 +1,16 @@
 #include <stdlib.h>
 
-#define page_size 4096;
 
+#ifndef page_size
+#define page_size 4096
+#endif
+
+
+#if page_size == 4096
+#define mask 13
+
+#elif page_size == 8192
+#define mask 16
+
+
+#endif
