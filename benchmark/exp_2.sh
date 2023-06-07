e945cd977f35e0cb38638b57b8c5d4525c7a93a1
勇敢冲就对 <2859201503@qq.com>
2023-06-07 14:42:12 +0000

    Upload New File

---
+++ benchmark/exp_2.sh
@@ -0,0 +1,28 @@
+gcc -o pt.out ./exp_1.c -lpthread
+gcc -o mem.out  ../mem_malloc/src/memmalloc.c  ./exp_1.c -DMEM_MALLOC   -I../mem_malloc/include -lpthread
+gcc -o hc.out  ../src/hcmalloc.c  ./exp_1.c -DHC_MALLOC   -I../src -lpthread
+
+
+i="0"
+BASE="10000"
+SLOP="10000"
+THNUM="20"
+
+while [ $i -lt 15 ]
+do
+    TIMES=$(($BASE+$i*$SLOP))
+    echo -n $TIMES" ">>hc
+    echo -n $TIMES" ">>pt
+    echo -n $TIMES" ">>mem
+    ./hc.out $TIMES $THNUM >>hc
+    ./pt.out $TIMES $THNUM >>pt
+    ./mem.out $TIMES $THNUM >>mem
+    echo "" >> hc
+    echo "" >> pt
+    echo "" >> mem
+    i=$(($i+1))
+done
+python3 static_analy.py 2
+rm hc
+rm pt
+rm mem
